749a94bf492a67ec30a18cdb9259da43f5627edf
MikeDr <mdresser71@gmail.com>
2018-09-22 23:42:33 -0500

    final updates

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,7 @@
-
-
-//Giphy api key
-
-//FCBAT7DCciFkcbxOqr0Yenvv9IqvDF4C
-
 //Initial array of TV shows 
 $(document).ready(function() {
 
-    var topics = ["The Duke's of Hazzard", "Buck Rogers", "Mork and Mindy", "Miami Vice", "Cheers", "Magnum P.I.", "", "Knight Rider", "", "Three's Company", "", "Night Court", "Mama's Family"];  
+    var topics = ["Moonlighting", "Buck Rogers", "Mork and Mindy", "Miami Vice", "ALF", "Magnum PI", "Knight Rider", "Three's Company", "Night Court", "Mama's Family"];  
   
     //  create topics array buttons
     function renderButtons(){
@@ -16,7 +10,7 @@ $(document).ready(function() {
       for (var i = 0; i < topics.length; i++) {
               //create all buttons
               var a = $('<button>');
-              a.addClass('1980sTV');
+              a.addClass('shows');
               a.attr('data-name', topics[i]);
               a.text(topics[i]);
               $('#buttons-view').append(a);
@@ -25,13 +19,13 @@ $(document).ready(function() {
           renderButtons();
   
   //on button click
-  $(document).on('click', '.1980sTV', function() {
+  $(document).on('click', '.shows', function() {
   
       //new variable will log the text data from each button
       var TVShows = $(this).html(); 
       // console.log(1980TV);
   
-      var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + TVShows + "&api_key=FCBAT7DCciFkcbxOqr0Yenvv9IqvDF4C";
+      var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + TVShows + "&limit=10&api_key=FCBAT7DCciFkcbxOqr0Yenvv9IqvDF4C";
       // console.log(queryURL);
   
       // Creating an AJAX call for the specific tv show button being clicked
@@ -43,7 +37,7 @@ $(document).ready(function() {
         var results = response.data;
           //console.log(results);
           //empties the div before adding more gifs
-          $('#tv-view').empty();
+          $('#TV-view').empty();
           for ( var j=0; j < results.length; j++) {
                       var imageDiv = $('<div>');
                       var imageView = results[j].images.fixed_height.url;
@@ -52,14 +46,14 @@ $(document).ready(function() {
   
           var gifImage = $('<img>').attr("src", still).attr('data-animate', imageView).attr('data-still', still);
                       gifImage.attr('data-state', 'still');
-                      $('#tv-view').prepend(gifImage);
+                      $('#TV-view').prepend(gifImage);
                       gifImage.on('click', playGif);
   
           // Pulling ratings for each tv show
           var rating = results[j].rating;
               // console.log(rating);
           var displayRated= $('<p>').text("Rating: " + rating);
-          $('#tv-view').prepend(displayRated);
+          $('#TV-view').prepend(displayRated);
     } // end for loop
   
   }); // done response
@@ -97,4 +91,3 @@ $(document).ready(function() {
           });
 
           });
-
